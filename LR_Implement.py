e6ec155bf1ea9423ccf85e79c7278ab4ea0fa6f5
Luz-Go <134108313+Luz-Go@users.noreply.github.com>
2023-12-13 03:36:05 +0000

    Add files via upload

--- LR_Implement.py
+++ LR_Implement.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-def lr_model(nvda):
+def lr(nvda):
+
     nvda = pd.read_csv("https://raw.githubusercontent.com/tiagopint0/Trabalho2PCD/main/SourceFile/NVDA.csv")
 
     nvda_reord = ["Open", "High", "Low", "Volume", "Close"]
@@ -33,24 +34,7 @@ def lr_model(nvda):
     rmse = (np.sqrt(mean_squared_error(Y_test, y_test_predict)))
     r2 = r2_score(Y_test, y_test_predict)
 
-    """
-    # Not good to illustrate the model performance
-
-    # Plotting the real vs. predicted values
-    plt.figure(figsize=(10, 5))
-
-    # Plotting the predicted values of the training set
-    plt.plot(Y_test, label="Real", color="blue", linestyle="dashed")
-
-    # Plotting the predicted values of the training set
-    plt.plot(X_test, label="Predictions", color="red")
-
-    plt.legend()
-    plt.title("LR Model: Real vs. Predicted")
-    plt.show()
-    """
-
-    # Printing the Model Evaluation
+       # Printing the Model Evaluation
     print("The model performance for testing set")
     print("--------------------------------------")
     print('RMSE is {}'.format(rmse))
@@ -78,5 +62,24 @@ def lr_model(nvda):
     else:
         print(f"The R2 score of {r2} is not close to 1, this indicates the model doesn't explain the variability in the prices well")
 
+    # Creating the Imputs for prediction
+    popen = input("\n Open: ")
+    phigh = input("\n High: ")
+    plow = input("\n Low: ")
+    pvolume = input("\n Volume: ")
+
+    # Applying the
+    spredict = np.array([[popen,phigh,plow,pvolume]])
+
+    nvda_predict = pd.DataFrame(spredict)
+
+    pred = lr.predict(nvda_predict)
+    print("\n")
+    print("\n")
+    print("The Price it's going to be :", pred)
+
 if __name__ == '__main__':
-    lr_model(nvda=pd.DataFrame)
+    lr(nvda=pd.DataFrame)
+
+
+#%%
